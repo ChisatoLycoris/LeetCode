e23d1432a5474253b2c4309eb475bfaa4941105c
ChisatoLycoris GitHub <changarm429@gmail.com>
2023-04-08 21:58:46 +0800

    LeetCode practice 7, 8, 11, 13

--- src/_5_PalindromicSubstring.java
+++ src/_5_PalindromicSubstring.java
@@ -13,6 +13,8 @@ import java.util.LinkedList;
  * Example 2:
  * Input: s = "cbbd"
  * Output: "bb"
+ * <br>
+ * <a href="https://leetcode.com/problems/longest-palindromic-substring/">5. Longest Palindromic Substring</a>
  */
 public class _5_PalindromicSubstring {
     /**
@@ -51,13 +53,12 @@ public class _5_PalindromicSubstring {
         int end = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            int left = i;
-            int right = i;
-            while (left >= 0 && s.charAt(left) == c) {
-                left--;
-            }
+            int left = i - 1;
+            int right = i + 1;
+            int j = 0;
             while (right < s.length() && s.charAt(right) == c) {
                 right++;
+                j++;
             }
             while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
                 left--;
@@ -68,17 +69,62 @@ public class _5_PalindromicSubstring {
                 end = right;
                 start = left;
             }
+            i = i + j;
         }
         return s.substring(start, end);
     }
 
     /**
      * Time Complexity: O(N^2)
+     * Space Complexity: O(N^2)
+     * <br>
+     * <a href=""></a>
+     */
+    public String dynamicPrograming(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] is true when s[i...j] is palindromic substring
+        // dp[i][j] = dp[i + 1][j - 1] && (s[i] == s[j])
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    /**
+     * Time Complexity: O(N^2)
      * Space Complexity: O(1)
      * <br>
-     * <a href="https://leetcode.com/problems/longest-palindromic-substring/submissions/929091516/"></a>
+     * <a href="https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm">Manacher Algorithm</a>
      */
-    public String longestPalindrome(String s) {
+    public String manacherAlgorithm(String s) {
 
         if (s.length() == 0) {
             throw new IllegalArgumentException("String must not be empty");
